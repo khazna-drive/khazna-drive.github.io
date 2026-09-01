8eb2aff472333769ac5b7742c521dc76c1cada94
a0545777031-ux <a0545777031@gmail.com>
2026-09-01 15:12:40 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'khazna-v1';
+const SHELL = ['./index.html', './admin.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (e.request.method !== 'GET' || url.origin !== location.origin) return;
+  e.respondWith(
+    fetch(e.request).then(r => { const copy = r.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return r; })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
