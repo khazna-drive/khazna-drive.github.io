cc9df5430fc5ab8a40f6d6367a008d3970283fab
a0545777031-ux <a0545777031@gmail.com>
2026-09-01 16:55:53 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'khazna-v1';
+const CACHE = 'daftar-v2';
 const SHELL = ['./index.html', './admin.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
